91fa4b256aa9b580b7c9bf57e661cada8e9e7423
Kapil Dahal <kapild@mtu.edu>
2021-06-14 12:44:08 -0400

    Initial Commit to kapilfreeman branch

---
+++ src/main/java/com/kapilfreeman/employeemanager/model/Employee.java
@@ -0,0 +1,12 @@
+package com.kapilfreeman.employeemanager.model;
+
+public class Employee {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phoneNumber;
+    private String imageUrl;
+
+}
